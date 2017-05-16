7015d7e50b3334c9efb0f8153f37713d6eb8b3e2
mesmers <171145448@qq.com>
2017-05-17 03:02:38 +0800

    s

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,37 +1,7 @@
-class l<T> {
-	private Object[] data = null;
-	private int front;
-	private int rear;
-
-	public l() {
-		this(100);
-	}
-
-	public l(int initialSize) {
-		data = new Object[initialSize];
-		front = rear = 0;
-	}
-
-	public boolean add(T e) {
-		data[rear++] = e;
-		return true;
-	}
-
-	public T poll() {
-		T value = (T) data[front];
-		data[front++] = null;
-		return value;
-	}
-
-	public int size() {
-		return rear - front;
-	}
-}
-
 public class BowlingGame {
 
 	private static final String strike = "X", spare = "/", miss = "-";
-
+	
 	private int getNum(String s) {
 		if (s.equals(strike))
 			return 10;
@@ -44,7 +14,7 @@ public class BowlingGame {
 		String[] m = bowlingCode.split("\\|");
 		int len = m.length;
 		int sum = 0;
-		l<Integer> d = new l<>();
+		int[] d = new int[2];
 		boolean flag = true;
 		for (int i = 0; i < len; i++) {
 			String cur = m[i];
@@ -53,35 +23,51 @@ public class BowlingGame {
 				continue;
 			}
 			if (i != len - 1 && cur.equals(strike)) {
-				int size = d.size();
+				int size = 2;
 				for (int j = 0; j < size; j++) {
-					int p = d.poll();
+					int p = d[j];
+					if(p==0)
+						continue;
 					if (p == 2) {
-						d.add(1);
+						d[j]=1;
+					}else{
+						d[j]=0;
 					}
 					sum += 10;
 				}
 				sum += 10;
-				d.add(2);
+				if(d[0]==0)
+					d[0]=2;
+				else
+					d[1]=2;
 				continue;
 			}
 			if (i != len - 1 && (cur.charAt(1) + "").equals(spare)) {
-				int size = d.size();
+				int size = 2;
 				for (int j = 0; j < size; j++) {
-					int p = d.poll();
+					int p = d[j];
+					if(p==0)
+						continue;
 					if (p == 2) {
 						sum += 10;
+						d[j]=0;
 					} else {
 						sum += getNum(cur.charAt(0) + "");
+						d[j]=0;
 					}
 				}
 				sum += 10;
-				d.add(1);
+				if(d[0]==0)
+					d[0]=1;
+				else
+					d[1]=1;
 				continue;
 			}
-			int size = d.size();
+			int size = 2;
 			for (int j = 0; j < size; j++) {
-				int p = d.poll();
+				int p = d[j];
+				if(p==0)
+					continue;
 				if (p == 2) {
 					if ((cur.length() > 1 && ((cur.charAt(1) + "")
 							.equals(spare))))
@@ -89,11 +75,13 @@ public class BowlingGame {
 					else
 						sum += getNum(cur.charAt(0) + "")
 								+ getNum(cur.charAt(1) + "");
+					d[j]=0;
 				} else {
 					if (cur.equals(strike))
 						sum += 10;
 					else
 						sum += getNum(cur.charAt(0) + "");
+					d[j]=0;
 				}
 			}
 			if (flag)
